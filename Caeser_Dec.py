5baa6f7c7c3644f7c483f43987ffc4d09792b22c
Ahmad Raed <wardehislam@hotmail.com>
2024-08-24 08:19:14 -0700

    Created The Main

---
+++ Caeser_Dec.py
@@ -0,0 +1,13 @@
+
+def Dec(text, shift):
+    new_word = ""
+    for i in text:
+        if i.isalpha():
+            start = ord('A') if i.isupper() else ord('a')
+
+            new_char = chr((ord(i) - start - shift) % 26 + start)
+            new_word += new_char
+        else:
+            new_word += i
+    return new_word
+
---
+++ FinalCaesar.py
@@ -0,0 +1,46 @@
+import os
+import logo
+import Caesar_Enc
+import Caeser_Dec
+
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def caesar(st):
+    if st == "encode":
+        text = input("Type your message: ")
+        shift = int(input("Type the shift number: "))
+        after_enc = Caesar_Enc.Enc(text, shift)
+        print(f"Here is the Encoded Result: {after_enc}\n")
+    elif st == "decode":
+        text = input("Type your message: ")
+        shift = int(input("Type the shift number: "))
+        after_dec = Caeser_Dec.Dec(text, shift)
+        print(f"Here is the Decoded Result: {after_dec}\n")
+    else:
+        return "wrong input!"
+
+print(logo)
+
+status = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()
+Possibility = caesar(status)
+
+if Possibility == "wrong input!":
+    print("You have two choices! But unfortunately you have lost them.")
+else:
+    while True:
+        clear_console()
+        print(logo)
+
+        At_Final = input("Type 'yes' if you want to go again. Otherwise type 'no'. ").lower()
+        if At_Final == "no":
+            break
+        elif At_Final == "yes":
+            status = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()
+            Possibility = caesar(status)
+            if Possibility == "wrong input!":
+                print("You have two choices! But unfortunately you have lost them.")
+                break
+        else:
+            print("You have two choices! But unfortunately you have lost them!")
+            break
